7f6f1822362865ef99194e6e1b6182bc4ac3dd54
tateys12 <tfollett53@gmail.com>
2023-04-13 11:53:42 -0230

    First Changes

---
+++ src/pages/shop/Shop.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Shop = () => {
+  return (
+    <div className="shop">
+      <div className="shoptitle">
+        <h1>Shop</h1>
+      </div>
+      <div className="products"></div>
+    </div>
+  );
+};
+
+export default Shop;
